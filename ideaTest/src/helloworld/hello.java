bac8e5470110eb8c197b895ee118f4da54c29a14
JSYPortable <574035907@qq.com>
2016-01-06 10:37:11 +0800

    annotation

---
+++ ideaTest/src/helloworld/hello.java
@@ -0,0 +1,25 @@
+package helloworld;
+import java.util.HashMap;
+
+public class hello{
+    public static void main(String args[]) {
+        // 向控制台输出信息
+        System.out.println("欢迎java01班的同学");
+    }
+    public void singleNumber() {
+//        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+//        for(int i=0;i<nums.length;i++){
+//            integer counter=map.get(nums[i]);
+//            if(counter==NULL){
+//                counter=new integer(0);
+//            }
+//            map.put(nums[i],++counter);
+//        }
+//        for(int i:map.keySet()){
+//            if(map.get(i)==1){
+//                return i;
+//            }
+//        }
+//        return -2147483648;
+    }
+}
